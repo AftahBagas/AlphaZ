1894f90fcd363c6f000aedd187acf927cc538016
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 23:53:21 +0700

    Update count.py

--- userbot/plugins/count.py
+++ userbot/plugins/count.py
@@ -5,7 +5,7 @@ from telethon.tl.custom import Dialog
 from telethon.tl.types import Channel, Chat, User
 
 from userbot import CMD_HELP
-from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PETERCORDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="stats$"))
@@ -64,25 +64,25 @@ async def stats(
         unread += dialog.unread_count
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
-    response = f"🔰**Stats for {full_name}**🔰\n\n"
-    response += f"😜 **Private Chats:** {private_chats} \n"
+    response = f"🎖**Stats for {full_name}**🔰\n\n"
+    response += f"⚡ **Private Chats:** {private_chats} \n"
     response += f"🔸   `Users: {private_chats - bots}` \n"
     response += f"🔹   `Bots: {bots}` \n"
-    response += f"😌 **Groups:** {groups} \n"
-    response += f"😌 **Channels:** {broadcast_channels} \n"
+    response += f"✨ **Groups:** {groups} \n"
+    response += f"✨ **Channels:** {broadcast_channels} \n"
     response += f"‼️  **Admin in Groups:** {admin_in_groups} \n"
-    response += f"🤣   `Creator: {creator_in_groups}` \n"
+    response += f"😎  `Creator: {creator_in_groups}` \n"
     response += f"❌   `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
     response += f"❌  **Admin in Channels:** {admin_in_broadcast_channels} \n"
     response += f"🔸   `Creator: {creator_in_channels}` \n"
     response += (
         f"🔹   `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
     )
-    response += f"🔥 **Unread:** {unread} \n"
-    response += f"🤘 **Unread Mentions:** {unread_mentions} \n\n"
+    response += f"🎖 **Unread:** {unread} \n"
+    response += f"🎖 **Unread Mentions:** {unread_mentions} \n\n"
     response += f"⚜️   __It Took:__ {stop_time:.02f}s \n"
     response += (
-        f"📌 **From The DataBase Of** :- [REBELBOT](https://t.me/REBELBOT_SUPPORT)"
+        f"📌 **From The DataBase Of** :- [SUPPORT(https://t.me/TEAMSquadUserbotSupport)"
     )
     await hell.edit(response)
 
