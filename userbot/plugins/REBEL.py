f1103f1f754afe6e310d4e278d88c1f7775defe8
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 21:53:40 +0700

    Update REBEL.py

--- userbot/plugins/REBEL.py
+++ userbot/plugins/REBEL.py
@@ -54,7 +54,7 @@ uptime = get_readable_time((time.time() - StartTime))
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="petercord$"))
-@bot.on(sudo_cmd(pattern="rebel$", allow_sudo=True))
+@bot.on(sudo_cmd(pattern="petercord$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
