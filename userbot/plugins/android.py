3906cbc44eb1a22c2cc75077120d761b5047b8f6
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 22:49:03 +0700

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -13,7 +13,7 @@ from requests import get
 from userbot.cmdhelp import CmdHelp
 
 from userbot import *
-from REBELBOT.utils import *
+from PETERCORDBOT.utils import *
 
 GITHUB = "https://github.com"
 DEVICES_DATA = (
