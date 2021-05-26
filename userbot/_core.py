f09f51447382a299ebe901a267480738eeb990bb
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 21:27:59 +0700

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -5,25 +5,25 @@ from pathlib import Path
 from telethon import events
 from telethon import functions, types
 from telethon.tl.types import InputMessagesFilterDocument
-from REBELBOT.utils import *
+from PETERCORDBOT.utils import *
 from userbot import *
-from userbot import bot as REBELBOT
+from userbot import bot as PETERCORDBOT
 
 DELETE_TIMEOUT = 5
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
-REBEL_logo = "./REBEL725/REBELBOT_logo.jpg"
-h1m4n5hu0p = REBELBOT.uid
-REBEL = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PETERCORD User"
+PETERCORD_logo = "./PETERCORD725/PETERCORSBOT_logo.jpg"
+h1m4n5hu0p = PETERCORDBOT.uid
+PETERCORD = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
 
-@REBELBOT.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
-@REBELBOT.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
+@PETERCORDBOT.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
 async def send(event):
     if event.fwd_from:
         return
     message_id = event.message.id
-    thumb = REBEL_logo
+    thumb = PETERCORD_logo
     input_str = event.pattern_match.group(1)
-    omk = f"**⍟ Plugin name ≈** `{input_str}`\n**⍟ Uploaded by ≈** {REBEL}\n\n⚡ **[LEGENDARY AF REBELBOT](t.me/REBELBOT_SUPPORT)** ⚡"
+    omk = f"**⍟ Plugin name ≈** `{input_str}`\n**⍟ Uploaded by ≈** {PETERCORD}\n\n⚡ **[LEGENDARY AF PETERCORDBOT](https://t.me/TEAMSquadUserbotSupport)** ⚡"
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         lauda = await event.client.send_file(
@@ -39,8 +39,8 @@ async def send(event):
     else:
         await edit_or_reply(event, "File not found..... Kek")
 
-@REBELBOT.on(admin_cmd(pattern="install$", outgoing=True))
-@REBELBOT.on(sudo_cmd(pattern="install$", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern="install$", outgoing=True))
+@PETERCORDBOT.on(sudo_cmd(pattern="install$", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -71,7 +71,7 @@ async def install(event):
                             a = "__Installing...__"
                             b = 1
                         await event.edit(a)
-                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {REBEL}\n\n{string}\n\n        ⚡ **[LEGENDARY AF REBELBOT](t.me/REBELBOT_SUPPORT)** ⚡", link_preview=False)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {PETERCORD}\n\n{string}\n\n        ⚡ **[LEGENDARY AF REBELBOT](t.me/REBELBOT_SUPPORT)** ⚡", link_preview=False)
                 return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
             else:
                 os.remove(downloaded_file_name)
@@ -80,8 +80,8 @@ async def install(event):
             await event.edit(f"**Failed to Install** \n`Error`\n{str(e)}")
             return os.remove(downloaded_file_name)
     
-@REBELBOT.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
-@REBELBOT.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
+@PETERCORDBOT.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
 async def uninstall(h1m4n5hu0p):
     if h1m4n5hu0p.fwd_from:
         return
@@ -94,8 +94,8 @@ async def uninstall(h1m4n5hu0p):
     except OSError as e:
         await h1m4n5hu0p.edit("Error: %s : %s" % (dir_path, e.strerror))
 
-@REBELBOT.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
-@REBELBOT.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
+@PETERCORDBOT.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
 async def unload(event):
     if event.fwd_from:
         return
@@ -111,8 +111,8 @@ async def unload(event):
         )
 
 
-@REBELBOT.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
-@REBELBOT.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
+@PETERCORDBOT.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
 async def load(event):
     if event.fwd_from:
         return
