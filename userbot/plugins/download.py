5078a1e9d2e192ddb50043baf1b557436d76c791
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 23:58:54 +0700

    Update download.py

--- userbot/plugins/download.py
+++ userbot/plugins/download.py
@@ -10,10 +10,10 @@ from datetime import datetime
 from pySmartDL import SmartDL
 
 from userbot import ALIVE_NAME, CMD_HELP
-from REBELBOT.utils import admin_cmd, humanbytes, progress, sudo_cmd
+from PETWRCORDBOT.utils import admin_cmd, humanbytes, progress, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PETERCORD User"
 
 
 @bot.on(admin_cmd(pattern="download(?: |$)(.*)", outgoing=True))
