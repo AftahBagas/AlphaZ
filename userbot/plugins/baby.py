386cff15104ed7b2ed6befd63d0de94d505ab512
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-26 23:22:12 +0700

    Update baby.py

--- userbot/plugins/baby.py
+++ userbot/plugins/baby.py
@@ -2,13 +2,13 @@
 # KEEP CREDITS ELSE GAY
 
 import random, re
-from REBELBOT.utils import admin_cmd
+from PETERCORDBOT.utils import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="baby ?(.*)"))
+@borg.on(admin_cmd(pattern="sayang ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("""HEYY BABY😍💏😘 HOW ARE  YOU  💏 AP KO PATA HA MA BASS AP KK LIYA HUU ISS DUNIYA MA ORR AP MERI JAAN HOO 💞💗😍🥰. ORR AP KKK BAAD KOI NAHI HAAA MERA 🥺🥺🥺. AP JAB NAHI HOO TII HO TABB KOI MERE SA BAAT  NAHI KARTA HAA 🥺🥺. BUT MERE KO ISS SA KOI FIRK NAHI PADTA HA😌😌. BASS AP MERA SATH HOO YAHI 🥰🥰. BHUT JADA HAA 💝❣️ I LOVE YOU MY SWEET HEART TUM MERI JAAN HO ORR MAA THUMARA. LIYA  KUCH VV KAR SAKTA HUUU """)
+        await event.edit("""HEYY Sayang, AKU SANGE MAU DING SEKALI AJA PLIS HIKS:) """)
         
 
