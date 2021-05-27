50d47d7f3df198dc9419698659c488eda35ebdfc
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 10:50:31 +0700

    Update history.py

--- userbot/plugins/history.py
+++ userbot/plugins/history.py
@@ -1,25 +1,25 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from REBELBOT import bot, CmdHelp
-from REBELBOT.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
+from PETERCORDBOT import bot, CmdHelp
+from PETERCORDBOT.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
 
-@REBELBOT.on(admin_cmd(pattern="history ?(.*)"))
-@REBELBOT.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
-async def _(REBELevent):
-    if REBELevent.fwd_from:
+@PETERCORDBOT.on(admin_cmd(pattern="history ?(.*)"))
+@PETERCORDBOT.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
+async def _(PETERCORDevent):
+    if PETERCORDevent.fwd_from:
         return 
-    if not REBELevent.reply_to_msg_id:
-       await eor(REBELevent, "`Please Reply To A User To Get This Module Work`")
+    if not PETERCORDevent.reply_to_msg_id:
+       await eor(PETERCORDevent, "`Please Reply To A User To Get This Module Work`")
        return
-    reply_message = await REBELevent.get_reply_message() 
+    reply_message = await PETERCORDevent.get_reply_message() 
     chat = "Sangmatainfo_bot"
     victim = reply_message.sender.id
     if reply_message.sender.bot:
-       await eor(REBELevent, "Need actual users. Not Bots")
+       await eor(PETERCORDevent, "Need actual users. Not Bots")
        return
-    await eor(REBELevent, "Checking...")
-    async with REBELevent.client.conversation(chat) as conv:
+    await eor(PETERCORDevent, "Checking...")
+    async with PETERCORDevent.client.conversation(chat) as conv:
           try:     
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
@@ -29,30 +29,30 @@ async def _(REBELevent):
               response2 = await response2 
               response3 = await response3 
           except YouBlockedUserError: 
-              await REBELevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              await PETERCORDevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
               return
           if response1.text.startswith("No records found"):
-             await eor(REBELevent, "User never changed his Username...")
+             await eor(PETERCORDevent, "User never changed his Username...")
           else: 
-             await REBELevent.delete()
-             await REBELevent.client.send_message(REBELevent.chat_id, response2.message)
+             await PETERCORDevent.delete()
+             await PETERCORDevent.client.send_message(PETERCORDevent.chat_id, response2.message)
 
-@REBELBOT.on(admin_cmd(pattern="unh ?(.*)"))
-@REBELBOT.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
-async def _(REBELevent):
-    if REBELevent.fwd_from:
+@PETERCORDBOT.on(admin_cmd(pattern="unh ?(.*)"))
+@PETERCORDBOT.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
+async def _(PETERCORDevent):
+    if PETERCORDevent.fwd_from:
         return 
-    if not REBELevent.reply_to_msg_id:
-       await eor(REBELevent, "`Please Reply To A User To Get This Module Work`")
+    if not PETERCORDevent.reply_to_msg_id:
+       await eor(PETERCORDevent, "`Please Reply To A User To Get This Module Work`")
        return
-    reply_message = await REBELevent.get_reply_message() 
+    reply_message = await PETERCORDevent.get_reply_message() 
     chat = "Sangmatainfo_bot"
     victim = reply_message.sender.id
     if reply_message.sender.bot:
-       await eor(REBELevent, "Need actual users. Not Bots")
+       await eor(PETERCORDevent, "Need actual users. Not Bots")
        return
-    await eor(REBELevent, "Checking...")
-    async with REBELevent.client.conversation(chat) as conv:
+    await eor(PETERCORDevent, "Checking...")
+    async with PETERCORDevent.client.conversation(chat) as conv:
           try:     
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
@@ -62,13 +62,13 @@ async def _(REBELevent):
               response2 = await response2 
               response3 = await response3 
           except YouBlockedUserError: 
-              await REBELevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              await PETERCORDevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
               return
           if response1.text.startswith("No records found"):
-             await eor(REBELevent, "User never changed his Username...")
+             await eor(PETERCORDevent, "User never changed his Username...")
           else: 
-             await REBELevent.delete()
-             await REBELevent.client.send_message(REBELevent.chat_id, response3.message)
+             await PETERCORDevent.delete()
+             await PETERCORDevent.client.send_message(REBELevent.chat_id, response3.message)
 
 CmdHelp("history").add_command(
   "history", "<reply to a user>", "Fetches the name history of replied user."
