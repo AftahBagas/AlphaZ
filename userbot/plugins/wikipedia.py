9306c0894f2002dc24b45fda9d928ddcaf360ea7
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 13:55:58 +0700

    Update wikipedia.py

--- userbot/plugins/wikipedia.py
+++ userbot/plugins/wikipedia.py
@@ -5,7 +5,7 @@
 Syntax: .wikipedia Query"""
 
 import wikipedia
-from REBELBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PETERCORDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="wikipedia (.*)"))
