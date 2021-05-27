cb4172cecf3b58783df64b0e8a99b8d4fbf7a9c0
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 12:10:14 +0700

    Update mask.py

--- userbot/plugins/mask.py
+++ userbot/plugins/mask.py
@@ -18,61 +18,61 @@ from userbot.helpers.functions import (
     iphonex,
     lolice,
 )
-from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PETERCORDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
 
 @bot.on(admin_cmd(pattern="mask$", outgoing=True))
 @bot.on(sudo_cmd(pattern="mask$", allow_sudo=True))
-async def _(REBELBOT):
-    reply_message = await REBELBOT.get_reply_message()
+async def _(PETERCORDBOT):
+    reply_message = await PETERCORDBOT.get_reply_message()
     if not reply_message.media or not reply_message:
-        await edit_or_reply(REBELBOT, "```reply to media message```")
+        await edit_or_reply(PETERCORDBOT, "```reply to media message```")
         return
     chat = "@hazmat_suit_bot"
     if reply_message.sender.bot:
-        await edit_or_reply(REBELBOT, "```Reply to actual users message.```")
+        await edit_or_reply(PETERCORDBOT, "```Reply to actual users message.```")
         return
-    event = await REBELBOT.edit("```Processing```")
-    async with REBELBOT.client.conversation(chat) as conv:
+    event = await PETERCORDBOT.edit("```Processing```")
+    async with PETERCORDBOT.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=905164246)
             )
-            await REBELBOT.client.send_message(chat, reply_message)
+            await PETERCORDBOT.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await edit_or_reply(REBELBOT, "`Please unblock` @hazmat_suit_bot `and try again`")
+            await edit_or_reply(PETERCORDBOT, "`Please unblock` @hazmat_suit_bot `and try again`")
             return
         if response.text.startswith("Forward"):
-            await edit_or_reply(REBELBOT, "```can you kindly disable your forward privacy settings for good?```"
+            await edit_or_reply(PETERCORDBOT, "```can you kindly disable your forward privacy settings for good?```"
             )
         else:
-            await REBELBOT.client.send_file(event.chat_id, response.message.media)
+            await PETERCORDBOT.client.send_file(event.chat_id, response.message.media)
             await event.delete()
 
 
 @bot.on(admin_cmd(pattern="awooify$", outgoing=True))
 @bot.on(sudo_cmd(pattern="awooify$", allow_sudo=True))
-async def REBELBOT(REBELmemes):
-    replied = await REBELmemes.get_reply_message()
+async def PETERCORDBOT(PETERCORDmemes):
+    replied = await PETERCORDmemes.get_reply_message()
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if not replied:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     if replied.media:
-        REBELevent = await edit_or_reply(REBELmemes, "passing to telegraph...")
+        PETERCORDevent = await edit_or_reply(PETERCORDmemes, "passing to telegraph...")
     else:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     try:
-        REBEL = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        REBEL = Get(REBEL)
-        await REBELmemes.client(REBEL)
+        PETERCORD = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        PETERCORD = Get(PETERCORD)
+        await PETERCORDmemes.client(PETERCORD)
     except BaseException:
         pass
-    download_location = await REBELmemes.client.download_media(
+    download_location = await PETERCORDmemes.client.download_media(
         replied, Config.TMP_DOWNLOAD_DIRECTORY
     )
     if download_location.endswith((".webp")):
@@ -80,47 +80,47 @@ async def REBELBOT(REBELmemes):
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await REBELevent.edit(
+            await PETERCORDevent.edit(
                 "the replied file size is not supported it must me below 5 mb"
             )
             os.remove(download_location)
             return
-        await REBELevent.edit("generating image..")
+        await PETERCORDevent.edit("generating image..")
     else:
-        await REBELevent.edit("the replied file is not supported")
+        await PETERCORDevent.edit("the replied file is not supported")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await REBELevent.edit("ERROR: " + str(exc))
+        await PETERCORDevent.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    REBEL = f"https://telegra.ph{response[0]}"
-    REBEL = await awooify(REBEL)
-    await REBELevent.delete()
-    await REBELmemes.client.send_file(REBELmemes.chat_id, REBEL, reply_to=replied)
+    PETERCORD = f"https://telegra.ph{response[0]}"
+    PETERCORD = await awooify(PETERCORD)
+    await PETERCORDevent.delete()
+    await PETERCORDmemes.client.send_file(PETERCORDmemes.chat_id, PETERCORD, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="lolice$"))
 @bot.on(sudo_cmd(pattern="lolice$", allow_sudo=True))
-async def REBELBOT(REBELmemes):
-    replied = await REBELmemes.get_reply_message()
+async def PETERCORDBOT(PETERCORDmemes):
+    replied = await PETERCORDmemes.get_reply_message()
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if not replied:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     if replied.media:
-        REBELevent = await edit_or_reply(REBELmemes, "passing to telegraph...")
+        PETERCORDevent = await edit_or_reply(PETERCORDmemes, "passing to telegraph...")
     else:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     try:
         REBEL = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
         REBEL = Get(REBEL)
-        await REBELmemes.client(REBEL)
+        await PETERCORDmemes.client(PETERCORD)
     except BaseException:
         pass
     download_location = await REBELmemes.client.download_media(
@@ -145,36 +145,36 @@ async def REBELBOT(REBELmemes):
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await REBELevent.edit("ERROR: " + str(exc))
+        await PETERCORDevent.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    REBEL = f"https://telegra.ph{response[0]}"
-    REBEL = await lolice(REBEL)
-    await REBELevent.delete()
-    await REBELmemes.client.send_file(REBELmemes.chat_id, REBEL, reply_to=replied)
+    PETERCORD = f"https://telegra.ph{response[0]}"
+    PETERCORD = await lolice(PETERCORD)
+    await PETERCORDevent.delete()
+    await PETERCORDmemes.client.send_file(PETERCORDmemes.chat_id, PETERCORD, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="bun$"))
 @bot.on(sudo_cmd(pattern="bun$", allow_sudo=True))
-async def REBELBOT(REBELmemes):
-    replied = await REBELmemes.get_reply_message()
+async def PETERCORDBOT(PETERCORDmemes):
+    replied = await PETERCORDmemes.get_reply_message()
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if not replied:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     if replied.media:
-        REBELevent = await edit_or_reply(REBELmemes, "passing to telegraph...")
+        PETERCORDmemes = await edit_or_reply(PETERCORDmemes, "passing to telegraph...")
     else:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     try:
-        REBEL = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        REBEL = Get(REBEL)
-        await REBELmemes.client(REBEL)
+        PETERCORD = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        PETERCORD = Get(PETERCORD)
+        await PETERCORDmemes.client(PETERCORD)
     except BaseException:
         pass
-    download_location = await REBELmemes.client.download_media(
+    download_location = await PETERCORDmemes.client.download_media(
         replied, Config.TMP_DOWNLOAD_DIRECTORY
     )
     if download_location.endswith((".webp")):
@@ -182,50 +182,50 @@ async def REBELBOT(REBELmemes):
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await REBELevent.edit(
+            await PETERCORDevent.edit(
                 "the replied file size is not supported it must me below 5 mb"
             )
             os.remove(download_location)
             return
-        await REBELevent.edit("generating image..")
+        await PETERCORDevent.edit("generating image..")
     else:
-        await REBELevent.edit("the replied file is not supported")
+        await PETERCORDevent.edit("the replied file is not supported")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await REBELevent.edit("ERROR: " + str(exc))
+        await PETERCORDevent.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    REBEL = f"https://telegra.ph{response[0]}"
-    REBEL = await baguette(REBEL)
-    await REBELevent.delete()
-    await REBELmemes.client.send_file(REBELmemes.chat_id, REBEL, reply_to=replied)
+    PETERCORD = f"https://telegra.ph{response[0]}"
+    PETERCORD = await baguette(PETERCORD)
+    await PETERCORDevent.delete()
+    await PETERCORDmemes.client.send_file(PETERCORDmemes.chat_id, PETERCORD, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="iphx$"))
 @bot.on(sudo_cmd(pattern="iphx$", allow_sudo=True))
-async def REBELBOT(REBELmemes):
-    replied = await REBELmemes.get_reply_message()
+async def REBELBOT(PETERCORDmemes):
+    replied = await PETERCORDmemes.get_reply_message()
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
     if not replied:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     if replied.media:
-        REBELevent = await edit_or_reply(REBELmemes, "passing to telegraph...")
+        PETERCORDevent = await edit_or_reply(PETERCORDmemes, "passing to telegraph...")
     else:
-        await edit_or_reply(REBELmemes, "reply to a supported media file")
+        await edit_or_reply(PETERCORDmemes, "reply to a supported media file")
         return
     try:
-        REBEL = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        REBEL = Get(REBEL)
-        await REBELmemes.client(REBEL)
+        PETERCORD = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        PETERCORD = Get(PETERCORD)
+        await PETERCORDmemes.client(PETERCORD)
     except BaseException:
         pass
-    download_location = await REBELmemes.client.download_media(
+    download_location = await PETERCORDmemes.client.download_media(
         replied, Config.TMP_DOWNLOAD_DIRECTORY
     )
     if download_location.endswith((".webp")):
@@ -233,27 +233,27 @@ async def REBELBOT(REBELmemes):
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await REBELevent.edit(
+            await PETERCORDevent.edit(
                 "the replied file size is not supported it must me below 5 mb"
             )
             os.remove(download_location)
             return
-        await REBELevent.edit("generating image..")
+        await PETERCORDevent.edit("generating image..")
     else:
-        await REBELevent.edit("the replied file is not supported")
+        await PETERCORDevent.edit("the replied file is not supported")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await REBELevent.edit("ERROR: " + str(exc))
+        await PETERCORDevent.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    REBEL = f"https://telegra.ph{response[0]}"
-    REBEL = await iphonex(REBEL)
-    await REBELevent.delete()
-    await REBELmemes.client.send_file(REBELmemes.chat_id, REBEL, reply_to=replied)
+    PETERCORD = f"https://telegra.ph{response[0]}"
+    PETERCORD = await iphonex(PETERCORD)
+    await PETERCORDevent.delete()
+    await PETERCORDmemes.client.send_file(PETERCORDmemes.chat_id, PETERCORD, reply_to=replied)
 
 
 CmdHelp("mask").add_command(
