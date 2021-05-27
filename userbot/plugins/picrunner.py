a5f62521dadb8d0db39a2134fe618db447dce4b4
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 12:57:58 +0700

    Update picrunner.py

--- userbot/plugins/picrunner.py
+++ userbot/plugins/picrunner.py
@@ -9,7 +9,7 @@ import urllib
 
 import requests
 from telethon.tl import functions
-from REBELBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PETERCORDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
