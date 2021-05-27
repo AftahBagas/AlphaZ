68dc30c9106d9d351448dc37d48d8deb2b304ba6
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 12:54:52 +0700

    Update packer.py

--- userbot/plugins/packer.py
+++ userbot/plugins/packer.py
@@ -3,12 +3,12 @@ import os
 import asyncio
 
 from userbot import CmdHelp
-from REBELBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot import bot as REBELBOT
+from PETERCORDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot import bot as PETERCORDBOT
 
 
-@REBELBOT.on(admin_cmd(pattern=r"unpack", outgoing=True))
-@REBELBOT.on(sudo_cmd(pattern=r"unpack"))
+@PETERCORDBOT.on(admin_cmd(pattern=r"unpack", outgoing=True))
+@PETERCORDBOT.on(sudo_cmd(pattern=r"unpack"))
 async def _(event):
     b = await event.client.download_media(await event.get_reply_message())
     a = open(b, "r")
@@ -23,8 +23,8 @@ async def _(event):
     os.remove(b)
 
 
-@REBELBOT.on(admin_cmd(pattern="repack ?(.*)", outgoing=True))
-@REBELBOT.on(sudo_cmd(pattern="repack ?(.*)", allow_sudo=True))
+@PETERCORDBOT.on(admin_cmd(pattern="repack ?(.*)", outgoing=True))
+@PETERCORDBOT.on(sudo_cmd(pattern="repack ?(.*)", allow_sudo=True))
 async def _(event):
     a = await event.get_reply_message()
     input_str = event.pattern_match.group(1)
