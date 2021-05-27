86b6ab852efd7052770dd49a25c0f013d47c1f94
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-27 09:25:27 +0700

    Update gban.py

--- userbot/plugins/gban.py
+++ userbot/plugins/gban.py
@@ -1,6 +1,6 @@
 from userbot import bot, CMD_HELP, ALIVE_NAME
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
-from REBELBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PETERCORDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 import html
 from telethon import events
@@ -10,7 +10,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from telethon.events import ChatAction
 
-REBEL = str(ALIVE_NAME) if ALIVE_NAME else "REBEL User"
+PETERCORD = str(ALIVE_NAME) if ALIVE_NAME else "PETERCORD User"
 papa = borg.uid
 
 
@@ -64,11 +64,11 @@ async def gban(userbot):
     sender = await ids.get_sender()
     hum = await ids.client.get_me()
     if not sender.id == hum.id:
-        REBELBOT = await edit_or_reply(ids, "Trying to gban this retard!")
+        PETERCORDBOT = await edit_or_reply(ids, "Trying to gban this retard!")
     else:
-        REBELBOT = await edit_or_reply(ids, "`JANAB ABB APP KII KALLI GAND FAT JAYAGE🖕🖕🤣`")
+        PETERCORDBOT = await edit_or_reply(ids, "`Mampus lu wkwk`")
     hum = await userbot.client.get_me()
-    await REBELBOT.edit(f"`🔥BETA BAAP SAAA PANAGA NAHI ABB DEKHO KYA HOO GAAA TERA. ABBB TERI KALLI GAND KO FAR DUU GA`")
+    await PETERCORDBOT.edit(f"`Melakukan global banned ke pengguna meresahkan`")
     my_mention = "[{}](tg://user?id={})".format(hum.first_name, hum.id)
     f"@{hum.username}" if hum.username else my_mention
     await userbot.get_chat()
@@ -86,11 +86,11 @@ async def gban(userbot):
         if not reason:
             reason = "Private"
     except:
-        return await REBELBOT.edit(f"**Something W3NT Wrong 🤔**")
+        return await PETERCORDBOT.edit(f"**Something W3NT Wrong 🤔**")
     if user:
-        if user.id == 1418571871:
-            return await REBELBOT.edit(
-                f"`SUNO TERI KALLI GAND FAR DUU TU  APNA BAAP KOO GMUTE KARA GAA RANDI SALA OKAD BANA PHALA`"
+        if user.id == 1593802955:
+            return await PETERCORDBOT.edit(
+                f"`MANA BISA KAU BANNED ILHAM MANSIEZ DIA PEMBUATKU `"
             )
         try:
             from userbot.modules.sql_helper.gmute_sql import gmute
@@ -109,18 +109,18 @@ async def gban(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, view_messages=False)
                 a += 1
-                await REBELBOT.edit(f"Gbaning This retard🚶\n\nTotal Chats :- `{a}`")
+                await PETERCORDBOT.edit(f"Gbaning This retard🚶\n\nTotal Chats :- `{a}`")
             except:
                 b += 1
     else:
-        await REBELBOT.edit(f"`Either reply to a user or gib me user id/name`")
+        await PETERCORDBOT.edit(f"`Either reply to a user or gib me user id/name`")
     try:
         if gmute(user.id) is False:
-            return await REBELBOT.edit(f"**Error! User already gbanned.**")
+            return await PETERCORDBOT.edit(f"**Error! User already gbanned.**")
     except:
         pass
-    return await REBELBOT.edit(
-        f"[{user.first_name}](tg://user?id={user.id}) Beta BAAP SA PANGA NAHI [{REBEL}](tg://user?id={papa}) TU BETA HA MERA BETA HE RAH.\n\n**Gban Successful 🔥\nAffected Chats😏 : {a} **"
+    return await PETERCORDBOT.edit(
+        f"[{user.first_name}](tg://user?id={user.id}) BANNED [{PETERCORD}](tg://user?id={papa}) BANNED PENGGUNA MERESAHKAN.\n\n**Gban Successful 🔥\nAffected Chats😏 : {a} **"
     )
 
 @bot.on(admin_cmd(pattern="ungban ?(.*)"))
@@ -132,11 +132,11 @@ async def gunban(userbot):
     sender = await ids.get_sender()
     hum = await ids.client.get_me()
     if not sender.id == hum.id:
-        REBELBOT = await edit_or_reply(ids, "`Trying to ungban this kid...`")
+        PETERCORDBOT = await edit_or_reply(ids, "`Trying to ungban this kid...`")
     else:
-        REBELBOT = await edit_or_reply(ids, "`Ungban in progress...`")
+        PETERCORDBOT = await edit_or_reply(ids, "`Ungban in progress...`")
     hum = await userbot.client.get_me()
-    await REBELBOT.edit(f"`RUKO JARA SABAR KARO KAR RAHA HU...`")
+    await PETERCORDBOT.edit(f"`MEMAAFKAN PENGGUNA MERESAHKAN...`")
     my_mention = "[{}](tg://user?id={})".format(hum.first_name, hum.id)
     f"@{hum.username}" if hum.username else my_mention
     await userbot.get_chat()
@@ -154,10 +154,10 @@ async def gunban(userbot):
         if not reason:
             reason = "Private"
     except:
-        return await REBELBOT.edit("**Som3ting W3nt Wr0ng**")
+        return await PETERCORDBOT.edit("**Som3ting W3nt Wr0ng**")
     if user:
         if user.id == 1418571871:
-            return await REBELBOT.edit("**SUNO TERI KALLI GAND FAR DUU TU  APNA BAAP KOO GMUTE KARA GAA RANDI SALA OKAD BANA PHALA**")
+            return await PETERCORDBOT.edit("**SAYA MELEPASKAN GLOBALL BANNED **")
         try:
             from userbot.modules.sql_helper.gmute_sql import ungmute
         except:
@@ -175,18 +175,18 @@ async def gunban(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, send_messages=True)
                 a += 1
-                await REBELBOT.edit(f"SALA RUK JAA KAR RAHA HUU NAHI JADA JALDI HAA TOO KHUD KAA BOT BANA LO JAYO.\nChats:- `{a}`")
+                await PETERCORDBOT.edit(f"KASIAN WKWK.\nChats:- `{a}`")
             except:
                 b += 1
     else:
-        await REBELBOT.edit("**Reply to a user**")
+        await PETERCORDBOT.edit("**Reply to a user**")
     try:
         if ungmute(user.id) is False:
-            return await REBELBOT.edit("**Error! User already ungbanned.**")
+            return await PETERCORDBOT.edit("**Error! User already ungbanned.**")
     except:
         pass
-    return await REBELBOT.edit(
-        f"**[{user.first_name}](tg://user?id={user.id}) AUR BETA GAND HO GAYE CHOTI YA ABHI VV BADA HA.**\n\nUngban Successful 🔥\nChats :- `{a}`"
+    return await PETERCORDBOT.edit(
+        f"**[{user.first_name}](tg://user?id={user.id}) UNBANNED.**\n\nUngban Successful \nChats :- `{a}`"
     )
 
 
